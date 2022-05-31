e06d20f97438e4f4660063748b1070635d03243b
UncleAyo <ayosamobitade@gmail.com>
2022-05-31 18:24:46 +0100

    some updates

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("pages.urls")),
+
     path('books', include('books.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
+
+    path('', include("pages.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
+class SignupPageView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
