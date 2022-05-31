e9c648be2280b2662bc7a047c402ea8e00b1c021
UncleAyo <ayosamobitade@gmail.com>
2022-05-31 17:01:36 +0100

    some updates

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views.generic import TemplateView
 # Create your views here.
 
-def home_page_view(request):
-    return HttpResponse("Hello World")
+class HomePageView(TemplateView):
+    template_name = 'home.html'
