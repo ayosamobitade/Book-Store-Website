7ade413c494bf894c484c99fec08aaabb323d6e8
UncleAyo <ayosamobitade@gmail.com>
2022-06-03 15:31:43 +0100

    website updates

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('users.urls')),
 
-    path('', include("pages.urls")),
+    path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
