6b410135d51af971321d4b591bd38cd17040334e
UncleAyo <ayosamobitade@gmail.com>
2022-06-15 07:48:37 +0100

    some update

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 from django.views.generic.base import TemplateView
 
 class OrdersPageView(TemplateView):
     template_name = 'orders/purchase.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
+
+        return context
 
 
