3f28bacccb656e812bbdcbfa0c32acd24ba52dc0
UncleAyo <ayosamobitade@gmail.com>
2022-06-15 08:06:58 +0100

    some update

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import OrdersPageView
+from .views import OrdersPageView, charge
+
 urlpatterns = [
     path('', OrdersPageView.as_view(), name = 'order'),
+    path('charge/', charge, name='charge' )
 ]
 
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
+import stripe
+from django.shortcuts import render
+
+stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
+
 
 # Create your views here.
 from django.views.generic.base import TemplateView
