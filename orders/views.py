76a351997b0fd5723e7c52c5937a9a1c7841cba9
UncleAyo <ayosamobitade@gmail.com>
2022-06-15 06:56:13 +0100

    some update

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import TemplateView
+
+class OrdersPageView(TemplateView):
+    template_name = 'orders/purchase.html'
+
+
