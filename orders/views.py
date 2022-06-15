0714c1be91fce8d3a2133aa7036d668d5562aa5d
UncleAyo <ayosamobitade@gmail.com>
2022-06-15 08:20:47 +0100

    some update

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 import stripe
 from django.shortcuts import render
+from django.contrib.auth.models import Permission
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
@@ -20,11 +21,20 @@ class OrdersPageView(TemplateView):
 
 
 def charge(request):
+    # get the permission
+    permission = Permission.objects.get(codename = 'special_status')
+    
+    # get user
+    u = request.user
+
+    # add to user's permission set
+    u.user_permissions.add(permission)
+
     if request.method == 'POST':
         charge = stripe.Charge.create(
             amount=3900, 
             currency = 'usd', 
-            description = 'Purchase all books'
+            description = 'Purchase all books',
             source = request.POST['stripeToken']
             )
-            return render(request, 'order/charge.html')
+        return render(request, 'order/charge.html')
